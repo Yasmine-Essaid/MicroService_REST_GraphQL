6aabb5cf6507cf5ae77f7fe36a5c0d2fcf8d0cd3
YasmineEssaid <YasmineEssaid@YASMINE-ESSAID>
2024-10-21 15:34:23 +0100

    Mon premier commit

---
+++ src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
@@ -0,0 +1,24 @@
+package org.sid.bankaccountservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bankaccountservice.enums.AccountType;
+
+import java.util.Date;
+@Builder
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private Double balance;
+    private String currency;
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+    @ManyToOne
+    private Customer customer;
+}
